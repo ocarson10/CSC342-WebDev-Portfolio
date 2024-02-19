8b7e7e3e210177f5562ebb1426a378ed6d545549
Olivia Carson <orcarson@ncsu.edu>
2024-02-19 15:12:13 -0500

    added hw3 setup

---
+++ Homework3/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
+
+const app = express();
+
+const PORT = 3000;
+const html_dir = __dirname + "/templates/";
+
+//app.use(express.static('public'));
+
+app.listen(PORT, () => console.log(`Sever listening on port: ${PORT}`));
+
+app.use(express.json());
+
+app.get('/', (req,res) =>{
+    res.sendFile(html_dir + 'success.html');
+})
